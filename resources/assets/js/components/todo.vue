6533550ddb42b2caa57bbe40c10f0561f49f1656
Nick Basile <nicholas.basile@outlook.com>
2017-12-05 19:19:54 -0600

    Styled todo component

--- resources/assets/js/components/todo.vue
+++ resources/assets/js/components/todo.vue
@@ -1,17 +1,20 @@
 <template>
-    <div>
-        <div>
-            <h1>Todo List</h1>
-            <div>
-                <input v-model="newTodo" placeholder="Add Todo">
-                <button @click="add" :disabled="newTodo.length === 0">Add</button>
+    <div class="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+        <div class="mb-4">
+            <h1 class="text-grey-darkest">Todo List</h1>
+            <div class="flex mt-4">
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker" v-model="newTodo" placeholder="Add Todo">
+                <button class="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-teal" @click="add" :disabled="newTodo.length === 0">Add</button>
             </div>
         </div>
         <div>
-            <div v-for="(todo, index) in todos" :key="todo.id">
-                <p>{{todo.text}}</p>
-                <button @click="updateStatus(todo)" v-text="todo.finished ? 'Not Done' : 'Done'"></button>
-                <button @click="remove(index)">Remove</button>
+            <div class="flex mb-4 items-center" v-for="(todo, index) in todos" :key="todo.id">
+                <p class="w-full" :class="todo.finished ? 'line-through text-green' : 'text-grey-darkest'">{{todo.text}}</p>
+                <button class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white" :class="todo.finished ? 'text-grey border-grey hover:bg-grey' : 'text-green border-green hover:bg-green'" @click="updateStatus(todo)" v-text="todo.finished ? 'Not Done' : 'Done'"></button>
+                <button class="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red" @click="remove(index)">Remove</button>
+            </div>
+            <div v-show="todos.length === 0">
+                <p class="w-full text-center text-grey-dark">There are no todos</p>
             </div>
         </div>
     </div>
