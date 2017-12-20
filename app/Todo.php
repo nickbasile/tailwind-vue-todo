a8b3aa0330c84fae73f79d34cc543a5ce9ae484e
Nick Basile <nicholas.basile@outlook.com>
2017-12-19 20:07:24 -0600

    Filled out our Todo model

--- app/Todo.php
+++ app/Todo.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    //
+    protected $fillable = [
+        'text',
+        'finished',
+    ];
+
+    protected $casts = [
+        'finished' => 'boolean',
+    ];
 }
