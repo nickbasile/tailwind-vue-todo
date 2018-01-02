de49806d63484f495210473944f64e2bba6f91d9
Nick Basile <nicholas.basile@outlook.com>
2018-01-02 16:56:46 -0600

    Removed create() and edit() from the TodoController

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -18,16 +18,6 @@ class TodoController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -52,17 +42,6 @@ class TodoController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Todo $todo)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Todo  $todo
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Todo $todo)
     {
         //
     }
